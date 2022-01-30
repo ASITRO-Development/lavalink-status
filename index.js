d0e530d176bb2734b8f2c8c9ec46efbeb8ea0156
Kartik OP <85619363+IndianDevInc@users.noreply.github.com>
2022-01-30 21:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ readdirSync("./Events/").forEach(file => {
 });
 
   
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('add this link in uptime')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
   client.login(token);
